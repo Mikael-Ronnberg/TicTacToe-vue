67103c8199a2a09bdcd940c3040ad13bdf28c993
Mikael Rönnberg <mikael.ronnberg@medieinstitutet.se>
2023-04-11 09:13:33 +0200

    Build: added square for better visualize

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 
+import Board from "./components/Board.vue";
+
 </script>
 
 <template>
+    <Board>
+        
+    </Board>
 
 </template>
 
--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,12 +1,41 @@
 <script setup lang="ts">
 
+const checkBoard = () =>{
+
+}
+
 </script>
 
 <template>
 
+<div class="board-container">
 
+    <div class="square"></div>
+    <div class="square"></div>
+    <div class="square"></div>
+    <div class="square"></div>
+    <div class="square"></div>
+    <div class="square"></div>
+    <div class="square"></div>
+    <div class="square"></div>
+    <div class="square"></div>
+</div>
+    
 </template>
 
 <style scoped>
+
+.square {
+    border: 1px solid black;
+    width: 100px;
+    height: 100px;
+    flex-basis: 33%;
+}
+.board-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+}
 
 </style>
